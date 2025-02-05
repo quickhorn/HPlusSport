8e78dba64cf2186d0ae05dbf186c4cd6cf9bc358
quickhorn <quickhorn@gmail.com>
2025-02-04 20:58:11 -0700

    Add project files.

---
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HPlusSport.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/Products")]
+        public void GetProducts()
+        {
+            
+        }
+
+        [HttpGet]
+        [Route("/Products/{id?}")]
+        public void GetProduct(int? id)
+        {
+            if (id == null)
+            {
+                return;
+            }
+        }
+    }
+}
